6a395d501872806c12f677e188c817d7c637fdc5
Vidhya Shankar <vidhyashankar.ril@gmail.com>
2026-03-19 00:38:55 +0530

    fix file casing issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -75,11 +75,11 @@
 
 // 
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "../pages/Login";
-import Signup from "../pages/Signup.jsx";
-import Dashboard from "../pages/Dashboard.jsx";
-import Income from "./pages/Income.jsx";
-import Expense from "./pages/Expense.jsx";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Income from "./pages/Income";
+import Expense from "./pages/Expense";
 import { useState } from "react";
 
 
